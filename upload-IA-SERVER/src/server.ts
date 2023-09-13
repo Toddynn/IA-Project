210f56321a31be55b7d63aae2034d3a5f8fa338a
Vinícius Gabriel Todis <viniciusgabriez07@gmail.com>
2023-09-13 16:44:18 -0300

    feat::sparkles: Setup Database

---
+++ upload-IA-SERVER/src/server.ts
@@ -0,0 +1,23 @@
+import FastifyCors from '@fastify/cors';
+import { fastify } from 'fastify';
+import { createCompletionRoute } from './routes/CompletionRoutes/create-completion';
+import { getAllPromptsRoute } from './routes/PromptsRoutes/get-all-prompts';
+import { getUniquePromptRoute } from './routes/PromptsRoutes/get-unique-prompt';
+import { createTranscriptionRoute } from './routes/TranscriptionRoutes/create-transcription';
+import { postVideoRoute } from './routes/VideoRoutes/post-video';
+
+const app = fastify();
+
+app.register(FastifyCors, {
+	origin: 'http://localhost:3333',
+});
+
+app.register(getAllPromptsRoute);
+app.register(getUniquePromptRoute);
+app.register(postVideoRoute);
+app.register(createTranscriptionRoute);
+app.register(createCompletionRoute);
+
+app.listen({ port: 3333 }).then(() => {
+	console.log('http server listening on port 3333');
+});
