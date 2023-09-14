48e10c36aafc1278f79abb9ea2f0d15f01d2b28a
Vinícius Gabriel Todis <viniciusgabriez07@gmail.com>
2023-09-14 17:30:28 -0300

    feat::sparkles: Create Component PromptForm

---
+++ upload-IA-WEB/src/components/Forms/PromptForm/index.tsx
@@ -0,0 +1,9 @@
+import PromptFormAction from './PromptFormAction';
+import PromptFormRoot from './PromptFormRoot';
+import PromptFormSelect from './PromptFormSelect';
+
+export const PromptForm = {
+	Root: PromptFormRoot,
+	Action: PromptFormAction,
+	Select: PromptFormSelect,
+};
