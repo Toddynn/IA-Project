7e1b664f3d3173b7905103ba8f7ba1f75f8dc3a9
Vinícius Gabriel Todis <viniciusgabriez07@gmail.com>
2023-09-14 17:30:10 -0300

    feat::sparkles: Create Component VideoFormRoot

---
+++ upload-IA-WEB/src/components/Forms/VideoForm/VideoFormRoot.tsx
@@ -0,0 +1,10 @@
+import { FormHTMLAttributes } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+export default function VideoFormRoot({ className, onSubmit, children, ...rest }: FormHTMLAttributes<HTMLFormElement>) {
+	return (
+		<form onSubmit={onSubmit} className={twMerge('space-y-6 ', className)} {...rest}>
+			{children}
+		</form>
+	);
+}
